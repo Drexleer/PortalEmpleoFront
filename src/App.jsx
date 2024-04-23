5c63db7eec01f1b535d1f7c2a6ee775d95a02be9
Edder Jose Alvarez Rodriguez <drexleer01@gmail.com>
2024-04-23 11:19:23 -0500

    Iniciando la aplicacion y Creando Carpetas

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Login from './Login/Login';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Login />
+    </>
+  );
+}
+
+export default App;
